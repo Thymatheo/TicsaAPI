1e035d4b3e66060a5b8168c086ba0ff2d48ee403
Dallaporta-Timote <timote.dallaporta@viacesi.fr>
2021-03-21 17:39:16 +0100

    Add all Bs

---
+++ TicsaAPI.BLL/BS/BsClient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TicsaAPI.BLL.BS.Interface;
+using TicsaAPI.DAL.DataProvider.Interface;
+using TicsaAPI.DAL.Models;
+
+namespace TicsaAPI.BLL.BS
+{
+    public class BsClient : BasicBs<Clients>, IBsClient
+    {
+        public BsClient(IBasicDp<Clients> dp) : base(dp)
+        {
+        }
+    }
+}
