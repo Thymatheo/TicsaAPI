97f2c4d28ac0a7ca4b60de90e6a6c3623fd0176d
Dallaporta-Timote <timote.dallaporta@viacesi.fr>
2021-03-22 19:37:55 +0100

    Update Db

---
+++ TicsaAPI.DAL/Migrations/20210322183729_FixEmailTypeProducer.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TicsaAPI.DAL.Migrations
+{
+    public partial class FixEmailTypeProducer : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Producer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Email",
+                table: "Producer",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
